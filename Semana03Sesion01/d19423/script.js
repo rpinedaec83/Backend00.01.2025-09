c3426badef25ccccfa62741dae7346b4e99d0b49
Roberto Pineda <roberto.pineda@convergia.com>
2025-10-13 20:37:11 -0500

    Clase 13102025 primera parte

---
+++ Semana03Sesion01/d19423/script.js
@@ -0,0 +1,6 @@
+console.log("Inicio de la applicacion");
+
+document.getElementById("btnClick01").addEventListener("click",(e)=>{
+    e.preventDefault();
+    alert("Otro Boton desde el archivo js")
+})
