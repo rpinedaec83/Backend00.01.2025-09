0719a30d517bc27930d08fc2b9be128b85a1eeea
Harold Benigno <hbenignomontero@gmail.com>
2025-11-28 20:09:57 -0500

    Implementación completa del CRUD de User (modelo, rutas y pruebas)

--- Hackaton09/SV71407231/src/routes/user.routes.js
+++ Hackaton09/SV71407231/src/routes/user.routes.js
@@ -2,15 +2,38 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-
+//obtener todos
 router.get("/", async (req, res) => {
     const users = await User.findAll();
     res.json(users);
 });
 
+// crear uno
 router.post("/", async (req, res) => {
-    const nuevo = await User.create(req.body);
-    res.json(nuevo);
+    const { nombre, correo } = req.body;
+    const newUser = await User.create({ nombre, correo });
+    res.json(newUser);
 });
+
+// obtener por id
+router.get("/:id", async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    res.json(user);
+});
+
+// actualizar
+router.put("/:id", async (req, res) => {
+    const { nombre, correo } = req.body;
+    await User.update({ nombre, correo }, { where: { id: req.params.id } });
+    res.json({ message: "Actualizado" });
+});
+
+// eliminar
+router.delete("/:id", async (req, res) => {
+    await User.destroy({ where: { id: req.params.id } });
+    res.json({ message: "Eliminado" });
+});
+
+
 
 module.exports = router;
--- Hackaton09/SV71407231/src/server/index.js
+++ Hackaton09/SV71407231/src/server/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { syncDB } = require("../config/db");
 
-require("../models");
+const userRoutes = require("../routes/user.routes");
 
 const app = express();
 
+require("../models");
+
+
+
 app.use(express.json());
+
+app.use("/users", userRoutes);
 
 async function start() {
     await syncDB();
