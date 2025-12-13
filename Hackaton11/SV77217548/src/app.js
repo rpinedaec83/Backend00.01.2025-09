62288cdff57cb3a59873802bfd46788421e7cec9
RonyChang <rony.ng.2011@gmail.com>
2025-12-13 17:53:45 -0500

    produccion, seed y docs terminado

--- Hackaton11/SV77217548/src/app.js
+++ Hackaton11/SV77217548/src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const materialRoute = require('./routes/material.route');
 const supplyRoute = require('./routes/supply.route');
 const employeeRoute = require('./routes/employee.route');
+const productionRoute = require('./routes/production.route');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
@@ -15,6 +16,7 @@ app.get('/', (req, res) => res.json({ok: true}));
 app.use('/api/materials', materialRoute);
 app.use('/api/supplies', supplyRoute);
 app.use('/api/employees', employeeRoute);
+app.use('/api/productions', productionRoute);
 
 app.use(errorHandler);
 
