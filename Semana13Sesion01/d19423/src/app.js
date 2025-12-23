764ed9e8c04f4e5f89cb88a8c39cd2bb62f3c149
Roberto Pineda <rpineda@x-codec.net>
2025-12-22 22:38:34 -0500

    Clase 22122025

---
+++ Semana13Sesion01/d19423/src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require("fs");
+const path = require('path');
+const helmet = require('helmet')
+const compression = require('compression');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+
+const logger = require('./moddlewares/logger');
+const error = require('./moddlewares/errorHandler');
+
+
+
+const app = express();
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+//app.use(morgan('combined', { stream: accessLogStream }))
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+app.use(cors({
+    origin: process.env.ORIGINS,
+    methods:["GET", "POST"]
+}))
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100
+})
+
+app.use(limiter)
+
+console.log(process.env.NODE_ENV)
+if(process.env.NODE_ENV==='production')
+    app.set('trust proxy',1)
+
+app.use(express.json({limit:'200kb'}));
+app.use(express.urlencoded({extended:true}))
+app.use(logger);
+app.use(error);
+
+
+module.exports = app;
---
+++ Semana13Sesion01/d19423/src/moddlewares/logger.js
@@ -0,0 +1,8 @@
+module.exports = function logger(req,res,next){
+    const start = process.hrtime.bigint();
+    res.on('finish',()=>{
+        const dur = Number(process.hrtime.bigint()-start)/1e6;
+        console.log(dur)
+    })
+    next();
+}
