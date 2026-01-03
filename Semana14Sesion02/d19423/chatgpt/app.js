accf46395fd05e41345f8388c1f86e7b60a4746b
Roberto Pineda <rpineda@x-codec.net>
2026-01-02 20:43:28 -0500

    Clase 02012026 segunda parte

--- Semana14Sesion02/d19423/chatgpt/app.js
+++ Semana14Sesion02/d19423/chatgpt/app.js
@@ -8,6 +8,22 @@ const server  = http.createServer(app);
 
 const PORT = process.env.PORT || 8080;
 
+const io = socketIO(server);
+
+io.on("connection",(socket)=>{
+    console.log("Nuevo Usuario conectado");
+    socket.on("disconnect",()=>console.log("Usuario Desconectado"));
+    socket.on("sendMessage", async(message, callback)=>{
+        try {
+            console.log(message)
+            socket.emit("message", message);
+            callback();
+        } catch (error) {
+            console.error(error)
+        }
+    })
+})
+
 app.use(express.static('public'));
 
 server.listen(PORT,()=>{
