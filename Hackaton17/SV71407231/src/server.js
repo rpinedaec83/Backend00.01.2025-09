285788fc8f47b6b69faa2a3cce17d79271afa332
Harold Benigno <hbenignomontero@gmail.com>
2026-01-26 00:24:41 -0500

    solucion puerto - render

--- Hackaton17/SV71407231/src/server.js
+++ Hackaton17/SV71407231/src/server.js
@@ -23,7 +23,7 @@ app.get('/db-test', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
